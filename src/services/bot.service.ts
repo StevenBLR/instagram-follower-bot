d223c060c89a402dd97d5672ec8b9b1362bc936b
StevenBLR_Mac <35036528+StevenBLR@users.noreply.github.com>
2021-10-14 22:34:04 +0200

    wip

--- src/services/bot.service.ts
+++ src/services/bot.service.ts
@@ -14,32 +14,50 @@ export default class BotService {
     }
 
     async run() {
-        const getWaitTime = () => (Math.random() * 5 * 60000) - (Math.random() * 1 * 60000);
+
+        const getWaitTime = () => (Math.random() * 5 * 10000) - (Math.random() * 1 * 10000);
+        await this.getSavedPosts();
         setInterval(async () => {
-            if (!this.usersToFollow || this.usersToFollow.length < 1) {
-                console.log('getting latest post likers');
-                await this.getLatestPostLikers();
-            } else {
-                console.log('we have ' + this.usersToFollow.length + ' users we can follow');
-                let user = this.usersToFollow.pop();
-                while (user.is_private) {
-                    user = this.usersToFollow.pop();
-                }
-                console.log('attempting to following user: ', user.username);
-                await this.follow(user.pk);
-            }
+            
+
+            // Process Ajout followers selon derniers likes 
+            // if (!this.usersToFollow || this.usersToFollow.length < 1) {
+            //     console.log('getting latest post likers');
+            //     await this.getLatestPostLikers();
+            // } else {
+            //     console.log('we have ' + this.usersToFollow.length + ' users we can follow');
+            //     let user = this.usersToFollow.pop();
+            //     while (user.is_private) {
+            //         user = this.usersToFollow.pop();
+            //     }
+            //     console.log('attempting to following user: ', user.username);
+            //     await this.follow(user.pk);
+            // }
         }, getWaitTime())
     }
 
-    async follow(userId: number) {
-        await this.ig.friendship.create(userId);
-    }
 
-    async getLatestPostLikers() {
-        const id = await this.ig.user.getIdByUsername(this.accountToParse);
+
+    // async follow(userId: number) {
+    //     await this.ig.friendship.create(userId);
+    // }
+
+    // async getLatestPostLikers() {
+    //     const id = await this.ig.user.getIdByUsername(this.accountToParse);
+    //     const feed = await this.ig.feed.user(id);
+    //     const posts = await feed.items();
+    //     this.usersToFollow = await (await this.ig.media.likers(posts[0].id)).users;
+    // }
+
+    async getSavedPosts(){
+        const id = await this.ig.user.getIdByUsername(this.user);
+        console.log(id)
         const feed = await this.ig.feed.user(id);
-        const posts = await feed.items();
-        this.usersToFollow = await (await this.ig.media.likers(posts[0].id)).users;
+        console.log(feed)
+        const acfolo = await this.ig.feed.accountFollowing();
+        console.log(acfolo)
+        const savedPosts = await feed.items();
+        console.log(savedPosts);
     }
 
     async login() {
